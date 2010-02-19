d6cd372abdadd28733455f5a62f6d6a6532afd4e
Siddhartha Reddy <sids@grok.in>
2010-02-19 17:07:13 +0530

    nerchuko.helpers/prepare-doc also handles String docs.

--- src/nerchuko/helpers.clj
+++ src/nerchuko/helpers.clj
@@ -1,7 +1,11 @@
 (ns nerchuko.helpers
-  (:use nerchuko.utils))
+  (:use nerchuko.utils
+        clj.text.tokenization))
 
 (defmulti prepare-doc class)
+
+(defmethod prepare-doc String [doc]
+  (prepare-doc (tokenize doc)))
 
 (defmethod prepare-doc clojure.lang.PersistentArrayMap [doc]
   (prepare-doc (flatten-map doc)))
