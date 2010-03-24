66ac420e33c8813370894bcc437a05956f174bd4
Siddhartha Reddy <sids@grok.in>
2010-03-24 15:54:30 +0530

    autodoc configuration.

--- project.clj
+++ project.clj
@@ -9,4 +9,9 @@
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [clj-text "0.0.2"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]])
+                     [lein-clojars "0.5.0-SNAPSHOT"]]
+  :autodoc {:name "Nerchuko"
+            :page-title "Nerchuko API Documentation"
+            :web-src-dir "http://github.com/sids/nerchuko/blob/"
+            :trim-prefix "nerchuko."
+            :copyright "Copyright 2010 Siddhartha Reddy"})
